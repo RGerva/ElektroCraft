e11bd7df0b87f8a291d368ca83736eb08adb66ab
RGerva <renan_gervatosky@hotmail.com>
2025-10-21 14:44:47 -0300

    Update spotless to 8.0.0

--- src/test/java/com/rgerva/elektrocraft/ElektroCraftTest.java
+++ src/test/java/com/rgerva/elektrocraft/ElektroCraftTest.java
@@ -14,9 +14,9 @@
 
 package com.rgerva.elektrocraft;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ElektroCraftTest {
 
